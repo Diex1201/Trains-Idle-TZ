2ea101fc8479945c8e42cd859d4cf0acfea1545e
Diex1201 <diexaka666@gmail.com>
2025-02-03 16:25:12 +0500

    Изменил метод поиска пути, убрал дублирование кода, сделал общий метод поиска пути, который будет принимать делегат эффективности поиска. Делегат по сути связывает методы расчета эффективности с методом поиска, давая ему определенную инструкцию того, что именно стоит искать.

--- Assets/Scripts/Path/Pathfinder.cs
+++ Assets/Scripts/Path/Pathfinder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 public class PathNode
@@ -14,62 +15,55 @@ public class Pathfinder
 {
     public List<Node> FindPathToMine(Node startNode, float trainSpeed, float trainMiningTime)
     {
-        Queue<PathNode> queue = new();
-        HashSet<Node> visited = new();
-        Dictionary<Node, float> distances = new();
-        Dictionary<Node, Node> previousNodes = new();
-        queue.Enqueue(new PathNode(startNode, 0));
-        visited.Add(startNode);
-        distances.Add(startNode, 0);
-
-        Node bestMine = null;
-        float maxEfficiency = float.MinValue;
-
-        while (queue.Count > 0)
+        Func<float, Node, float> calculateMineEfficiency = (distance, node) =>
         {
-            PathNode currentPathNode = queue.Dequeue();
-            Node current = currentPathNode.Node;
-            float currentDistance = currentPathNode.Distance;
-
-            if (current.NodeType == NodeType.Mine)
+            if (node.NodeType == NodeType.Mine)
             {
-                MineNode mineNode = current as MineNode;
-                float miningTimeMultiplier = mineNode.MiningTimeMultiplier;
-                float efficiency = CalculateMineEfficiency(currentDistance, miningTimeMultiplier, trainSpeed, trainMiningTime);
-                if (bestMine == null)
-                {
-                    maxEfficiency = efficiency;
-                    bestMine = current;
-                }
-                else if (!float.IsNaN(efficiency) && !float.IsInfinity(efficiency) && efficiency > maxEfficiency)
-                {
-                    maxEfficiency = efficiency;
-                    bestMine = current;
-                }
-                continue;
+                MineNode mineNode = node as MineNode;
+                return CalculateMineEfficiency(distance, mineNode.MiningTimeMultiplier, trainSpeed, trainMiningTime);
             }
-            foreach (Neighbour neighbour in current.Neighbours)
-            {
-                if (!visited.Contains(neighbour.NeighbourNode))
-                {
-                    visited.Add(neighbour.NeighbourNode);
-                    distances.Add(neighbour.NeighbourNode, currentDistance + neighbour.DistanceToNeighbourNode);
-                    previousNodes.Add(neighbour.NeighbourNode, current);
-                    queue.Enqueue(new PathNode(neighbour.NeighbourNode, currentDistance + neighbour.DistanceToNeighbourNode));
-                }
-            }
-        }
-        if (bestMine != null)
-        {
-            return ReconstructPath(bestMine, previousNodes);
-        }
-        else
-        {
-            Debug.LogWarning("No mine found!");
-            return null;
-        }
+            return float.NegativeInfinity;
+        };
+        return FindPath(startNode, trainSpeed, calculateMineEfficiency);
     }
     public List<Node> FindPathToBase(Node startNode, float trainSpeed)
+	{
+        Func<float, Node, float> calculateBaseEfficiency = (distance, node) =>
+        {
+            if (node.NodeType == NodeType.Base)
+            {
+                BaseNode baseNode = node as BaseNode;
+                return CalculateBaseEfficiency(distance, baseNode.ResourcesMultiplier, trainSpeed);
+            }
+            return float.NegativeInfinity;
+        };
+        return FindPath(startNode, trainSpeed, calculateBaseEfficiency);
+    }
+    private float CalculateMineEfficiency(float distance, float miningTimeMultiplier, float trainSpeed, float trainMiningTime)
+    {
+        float travelTime = distance / trainSpeed;
+        float totalTime = travelTime + trainMiningTime * miningTimeMultiplier;
+        if (totalTime <= 0) return float.NegativeInfinity;
+        return 1 / totalTime;
+    }
+    private float CalculateBaseEfficiency(float distance, float resourceMultiplier, float trainSpeed)
+    {
+        float travelTime = distance / trainSpeed;
+        if (travelTime <= 0) return float.NegativeInfinity;
+        return resourceMultiplier / travelTime;
+    }
+    private List<Node> ReconstructPath(Node targetNode, Dictionary<Node, Node> previousNodes)
+    {
+        List<Node> path = new();
+        Node current = targetNode;
+        while (current != null && previousNodes.ContainsKey(current))
+        {
+            path.Insert(0, current);
+            current = previousNodes[current];
+        }
+        return path;
+    }
+    public List<Node> FindPath(Node startNode, float trainSpeed, Func<float, Node, float> calculateEfficiency)
 	{
         Queue<PathNode> queue = new();
         HashSet<Node> visited = new();
@@ -78,72 +72,41 @@ public class Pathfinder
         queue.Enqueue(new PathNode(startNode, 0));
         visited.Add(startNode);
         distances.Add(startNode, 0);
-        Node bestBase = null;
-        float maxEfficiency = float.MinValue;
+        Node bestNode = null;
+        float maxEfficiency = float.NegativeInfinity;
         while (queue.Count > 0)
         {
             PathNode currentPathNode = queue.Dequeue();
-            Node current = currentPathNode.Node;
+            Node currentNode = currentPathNode.Node;
             float currentDistance = currentPathNode.Distance;
-            if (current.NodeType == NodeType.Base)
+
+            float efficiency = calculateEfficiency(currentDistance, currentNode);
+
+            if (efficiency > maxEfficiency)
             {
-                BaseNode baseNode = current as BaseNode;
-                float efficiency = CalculateBaseEfficiency(currentDistance, baseNode.ResourcesMultiplier, trainSpeed);
-                if (bestBase == null)
-                {
-                    maxEfficiency = efficiency;
-                    bestBase = current;
-                }
-                else if (!float.IsNaN(efficiency) && !float.IsInfinity(efficiency) && efficiency > maxEfficiency)
-                {
-                    maxEfficiency = efficiency;
-                    bestBase = current;
-                }
-                continue;
+                maxEfficiency = efficiency;
+                bestNode = currentNode;
             }
-            foreach (Neighbour neighbour in current.Neighbours)
+
+            foreach (Neighbour neighbour in currentNode.Neighbours)
             {
                 if (!visited.Contains(neighbour.NeighbourNode))
                 {
                     visited.Add(neighbour.NeighbourNode);
                     distances.Add(neighbour.NeighbourNode, currentDistance + neighbour.DistanceToNeighbourNode);
-                    previousNodes.Add(neighbour.NeighbourNode, current);
+                    previousNodes.Add(neighbour.NeighbourNode, currentNode);
                     queue.Enqueue(new PathNode(neighbour.NeighbourNode, currentDistance + neighbour.DistanceToNeighbourNode));
                 }
             }
         }
-        if (bestBase != null)
+        if (bestNode != null)
         {
-            return ReconstructPath(bestBase, previousNodes);
+            return ReconstructPath(bestNode, previousNodes);
         }
         else
         {
-            Debug.LogWarning("No base found!");
+            Debug.LogWarning("No target node found!");
             return null;
         }
-    }
-    private float CalculateMineEfficiency(float distance, float miningTimeMultiplier, float trainSpeed, float trainMiningTime)
-    {
-        float travelTime = distance / trainSpeed;
-        float totalTime = travelTime + trainMiningTime * miningTimeMultiplier;
-        if (totalTime == 0) return 0;
-        if (travelTime == 0 || trainMiningTime == 0) return 0;
-        return 1 / totalTime;
-    }
-    private float CalculateBaseEfficiency(float distance, float resourceMultiplier, float trainSpeed)
-    {
-        float travelTime = distance / trainSpeed;
-        return resourceMultiplier / travelTime;
-    }
-    private List<Node> ReconstructPath(Node targetNode, Dictionary<Node, Node> previousNodes)
-    {
-        List<Node> path = new List<Node>();
-        Node current = targetNode;
-        while (current != null && previousNodes.ContainsKey(current))
-        {
-            path.Insert(0, current);
-            current = previousNodes[current];
-        }
-        return path;
     }
 }
